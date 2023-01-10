fd1cecfd88cc29c6d85c68cb808d7c5c03e38bc7
Tetiana2023 <drofaj18@gmail.com>
2023-01-10 11:46:22 +0200

    task -01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryNewEl = galleryItems.map(item => {
+    return ` <a class= "gallery__link" href= "${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`
+})
+.join("");
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryNewEl);
+console.log(galleryEl);
+
+
+const gallery = new SimpleLightbox('.gallery a', 
+{ captions: true,
+    captionsData: "alt",
+    captionDelay: 250,});
