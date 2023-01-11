3432767e2e4b0f460d270cefc5cd40075ec1f0bc
Tetiana2023 <drofaj18@gmail.com>
2023-01-11 21:54:26 +0200

    task -03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,13 +4,32 @@ const feedbackFormEl = document.querySelector(".feedback-form")
 const inputEl = document.querySelector("input");
 const textareaEl = document.querySelector("textarea");
 
-const object = {};
+rezultmessageFormdata();
+
+const formData = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
 
 feedbackFormEl.addEventListener('submit', onSubmitForm);
+feedbackFormEl.addEventListener('input', throttle(onFormInput), 500);
 
 function onSubmitForm(event){
     event.preventDefault();
     event.target.reset();
-    
+    localStorage.removeItem("feedback-form-state");
+    console.log(formData);
+
+}
+function onFormInput(event){
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+
+function rezultmessageFormdata(){
+    const message = localStorage.getItem("feedback-form-state");
+    const savedMessage = JSON.parse(message);
+
+    if (savedMessage) {
+       inputEl.value = savedMessage.email || '';
+       textareaEl.value = savedMessage.message || '';
+    }
 
 }
