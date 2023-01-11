4974c41eea5edd3772a50024b14eb6f6f0fce6a3
Tetiana2023 <drofaj18@gmail.com>
2023-01-11 15:17:47 +0200

    task-03 start

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
+import throttle from "lodash.throttle";
+ 
+const feedbackFormEl = document.querySelector(".feedback-form")
+const inputEl = document.querySelector("input");
+const textareaEl = document.querySelector("textarea");
 
+const object = {};
+
+feedbackFormEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event){
+    event.preventDefault();
+    event.target.reset();
+    
+
+}
