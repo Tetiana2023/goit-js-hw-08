8b4a3f92d0ed50e038bae98e9025cc94ebfc032f
Tetiana2023 <drofaj18@gmail.com>
2023-01-12 20:15:54 +0200

    task -02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
+import Player from "@vimeo/player";
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+  
+    player.on('timeupdate', throttle(onPlayed, 1000));
+
+    function onPlayed(event){
+        localStorage.setItem("videoplayer-current-time", event.seconds)
+    }
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0);
+
+
+    
+    // || 0
+
+    // ).catch(function(error) {
+        // console.log(error)
+
+    // });
+
+   // player.setCurrentTime(30.456).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    //}).catch(function(error) {
+      //  switch (error.name) {
+        //    case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+          //      break;
+    
+            //default:
+                // some other error occurred
+              //  break;
+        //}
+    //});
